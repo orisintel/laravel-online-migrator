1d551c93fb29f02c1159168e8f85c9824deb73e3
Paul Rogers <progers@orisintel.com>
2018-09-13 17:39:01 -0400

    docs(config/online-migrator.php): Included comments to aid those who come after.

--- config/online-migrator.php
+++ config/online-migrator.php
@@ -1,9 +1,51 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Option To Force Or Bypass OnlineMigrator
+    |--------------------------------------------------------------------------
+    |
+    | When not specified it will use the on-line capabilities; except for DBs
+    | with 'test' in their name.
+    |
+    | '0' bypasses any on-line tools and sends queries unchanged.
+    | '1' (or any truthy value) forces use of the on-line tools.
+    |
+    */
+
     'enabled' => env('USE_ONLINE_MIGRATOR'),
 
+    /*
+    |--------------------------------------------------------------------------
+    | On-line Strategy
+    |--------------------------------------------------------------------------
+    |
+    | Which tool or technique to use when trying to minimize disruptions during
+    | migrations. Individual migrations may override this with traits.
+    |
+    | Currently there are only two values:
+    | pt-online-schema-change
+    | innodb-online-ddl
+    |
+    */
+
     'strategy' => env('ONLINE_MIGRATOR_STRATEGY', 'pt-online-schema-change'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Percona Online Schema Change Options
+    |--------------------------------------------------------------------------
+    |
+    | Accepts a comma-separated list of options for pt-online-schema-change.
+    |
+    | These options are always included unless overwritten with different
+    | values:
+    | --alter-foreign-keys-method=auto
+    | --no-check-alter
+    | --no-check-unique-key-change
+    |
+    */
 
     'ptosc-options' => env('PTOSC_OPTIONS'),
 ];
